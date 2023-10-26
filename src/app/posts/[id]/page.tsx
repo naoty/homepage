275203edd7d6d8f25e1c31214860f93c9db0feb2
Naoto Kaneko <naoty.k@gmail.com>
2023-10-26 20:57:03 +0900

    ブログ記事を表示する

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,8 +1,12 @@
+import dynamic from "next/dynamic"
+
 export default function Post({ params }: { params: { id: string } }) {
+  const Content = dynamic(() => import(`@/contents/app/posts/${params.id}/post.md`))
+
   return (
     <main className='col-start-2 row-start-2 container'>
       <article className='prose'>
-        <h1>Post #{params.id}</h1>
+        <Content />
       </article>
     </main>
   )
