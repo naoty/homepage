2fc9b792c7c81450f1c62f1cc133fb7a1d9115f8
Naoto Kaneko <naoty.k@gmail.com>
2024-01-07 13:18:16 +0900

    余白を調整する

--- src/app/shogi/page.tsx
+++ src/app/shogi/page.tsx
@@ -9,9 +9,9 @@ export default function ShogiNotes() {
   return (
     <main className='grid grid-cols-mobile md:grid-cols-desktop grid-rows-mobile md:grid-rows-desktop'>
       <header className='col-start-2 row-start-2 container'>
-        <h1 className='text-3xl font-bold mb-4 pb-4 md:pb-8'>将棋メモ</h1>
+        <h1 className='text-3xl font-bold pb-4 md:pb-8'>将棋メモ</h1>
       </header>
-      <section className='col-start-2 row-start-3 container pb-4 max-md:pt-12 md:pb-8 md:pr-4'>
+      <section className='col-start-2 row-start-3 container pb-4 md:pb-8 md:pr-4'>
         <ul className='flex flex-col space-y-2'>
           {notes.map(note => (
             <li key={note.id} className='flex flex-row space-x-2 items-center'>
