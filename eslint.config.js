4328e9d4256b66c60421f381c007bd861224edbd
Naoto Kaneko <naoty.k@gmail.com>
2024-10-05 23:26:17 +0900

    prettierをセットアップする

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,9 @@
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
 );
