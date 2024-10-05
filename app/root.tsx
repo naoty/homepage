9ef1df269d9b42c273084a83a5658e712a012252
Naoto Kaneko <naoty.k@gmail.com>
2024-10-05 18:18:55 +0900

    React Routerの起動に必要最小限のファイルを追加する

---
+++ app/root.tsx
@@ -0,0 +1,13 @@
+export default function Root() {
+  return (
+    <html lang="ja">
+      <head>
+        <meta charSet="utf-8" />
+        <title>Naoto Kaneko</title>
+      </head>
+      <body>
+        <h1>Hello react-router!</h1>
+      </body>
+    </html>
+  );
+}
