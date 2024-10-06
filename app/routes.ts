08ccedc6c840e47b81ccd3d9f9fc1b1dd073c2a5
Naoto Kaneko <naoty.k@gmail.com>
2024-10-06 22:42:38 +0900

    /postsにルートを追加する

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,10 @@
-import { index } from "node_modules/@react-router/dev/dist/config/routes";
+import {
+  index,
+  route,
+  type RouteConfig,
+} from "node_modules/@react-router/dev/dist/config/routes";
 
-export const routes = [index("routes/home.tsx")];
+export const routes: RouteConfig = [
+  index("routes/home.tsx"),
+  route("posts", "routes/posts.tsx", [index("routes/posts/home.tsx")]),
+];
