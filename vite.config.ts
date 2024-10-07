3f685149893c43a9feb25d087e910292ce31d37e
Naoto Kaneko <naoty.k@gmail.com>
2024-10-08 08:59:19 +0900

    vite-plugin-markdownをセットアップする

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
+import { Mode, plugin as mdPlugin } from "vite-plugin-markdown";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [reactRouter(), tsconfigPaths(), mdPlugin({ mode: [Mode.HTML] })],
 });
