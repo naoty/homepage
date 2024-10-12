5c98c74a1abecd6a2943b0db6beab57193a567f7
Naoto Kaneko <naoty.k@gmail.com>
2024-10-13 08:55:06 +0900

    pre-renderingの設定を追加する

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { reactRouter } from "@react-router/dev/vite";
+import { globSync } from "fs";
+import { dirname } from "path";
 import { defineConfig } from "vite";
 import { Mode, plugin as mdPlugin } from "vite-plugin-markdown";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths(), mdPlugin({ mode: [Mode.HTML] })],
+  plugins: [
+    reactRouter({
+      async prerender() {
+        const postPaths = globSync("./contents/posts/**/post.md")
+          .map((path) => dirname(path).split("/").pop())
+          .map((id) => `/posts/${id}`);
+        return ["/", "/posts", ...postPaths];
+      },
+    }),
+    tsconfigPaths(),
+    mdPlugin({ mode: [Mode.HTML] }),
+  ],
 });
