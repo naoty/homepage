918e9aaa93b2dcb2297c33124569fee76398f601
Naoto Kaneko <naoty.k@gmail.com>
2024-10-12 14:58:45 +0900

    コンテンツから記事一覧を生成する

--- app/routes/posts/home.tsx
+++ app/routes/posts/home.tsx
@@ -1,32 +1,54 @@
+import * as path from "path";
 import Container from "~/components/container";
 import Link from "~/components/link";
+import type * as Route from "./+types.posts";
 
-export default function Home() {
+interface Post {
+  attributes: Frontmatter;
+  html: string;
+}
+
+interface Frontmatter {
+  title: string;
+  time: string;
+  tags: string[];
+}
+
+interface PostAttribute {
+  id: number;
+  title: string;
+  time: string;
+  tags: string[];
+}
+
+export function loader() {
+  const posts = import.meta.glob<Post>("../../../contents/posts/**/*.md", {
+    eager: true,
+  });
+  const attributes: PostAttribute[] = Object.keys(posts)
+    .map((postPath) => {
+      const dir = path.dirname(postPath);
+      const id = parseInt(dir.split("/").pop()!);
+      const { attributes } = posts[postPath];
+      return { id, ...attributes };
+    })
+    .sort((a, b) => b.id - a.id);
+  return { attributes };
+}
+
+export default function Home({ loaderData }: Route.ComponentProps) {
   return (
-    <main className="grid grid-cols-8 gap-x-6 pt-6">
+    <main className="grid grid-cols-8 gap-x-6 py-6">
       <Container className="col-span-4 col-start-3">
-        <h1 className="border-rails-border-main border-b pb-3 text-3xl font-bold">
+        <h1 className="border-b border-rails-border-main pb-3 text-3xl font-bold">
           Posts
         </h1>
         <ul className="list-disc pl-6">
-          <li>
-            <Link href="">RemixからFirebase Authenticationで認証する</Link>
-          </li>
-          <li>
-            <Link href="">スマホからブログを書きたい</Link>
-          </li>
-          <li>
-            <Link href="">Full Stack Web Development with Remix</Link>
-          </li>
-          <li>
-            <Link href="">Windowsのキーマッピング3</Link>
-          </li>
-          <li>
-            <Link href="">sealed classとの付き合い方を考える</Link>
-          </li>
-          <li>
-            <Link href="">複雑性はチリツモであるという話</Link>
-          </li>
+          {loaderData.attributes.map((attribute) => (
+            <li key={attribute.id}>
+              <Link href={`/posts/${attribute.id}`}>{attribute.title}</Link>
+            </li>
+          ))}
         </ul>
       </Container>
     </main>
