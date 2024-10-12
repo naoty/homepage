a0558fa40be70aa2ab09b2a096601f826f9ad657
Naoto Kaneko <naoty.k@gmail.com>
2024-10-13 00:49:06 +0900

    画面遷移時にリンク横にインジケータを表示する

--- app/components/link.tsx
+++ app/components/link.tsx
@@ -1,17 +1,24 @@
+import { Loader2 } from "lucide-react";
 import { type AnchorHTMLAttributes } from "react";
-import { Link as ReactRouterLink } from "react-router";
+import { Link as ReactRouterLink, useNavigation } from "react-router";
 import { twMerge } from "tailwind-merge";
 
 export default function Link(props: AnchorHTMLAttributes<HTMLAnchorElement>) {
+  const navigation = useNavigation();
+  const isLoading =
+    navigation.state !== "idle" && navigation.location.pathname === props.href;
   const classNames = twMerge(
     "text-rails-link underline hover:bg-rails-link hover:text-white hover:no-underline",
     props.className,
   );
 
   return typeof props.href === "string" ? (
-    <ReactRouterLink to={props.href} className={classNames}>
-      {props.children}
-    </ReactRouterLink>
+    <span className="inline-flex items-center space-x-1">
+      <ReactRouterLink to={props.href} className={classNames}>
+        {props.children}
+      </ReactRouterLink>
+      {isLoading && <Loader2 className="inline size-4 animate-spin" />}
+    </span>
   ) : (
     <span className={classNames}>{props.children}</span>
   );
