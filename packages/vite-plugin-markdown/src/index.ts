37a1daa6b79f4694dd57a175d539d2af3f061267
Naoto Kaneko <naoty.k@gmail.com>
2024-10-24 11:41:24 +0900

    markdownを空のコードに変換する

--- packages/vite-plugin-markdown/src/index.ts
+++ packages/vite-plugin-markdown/src/index.ts
@@ -1,3 +1,22 @@
-export function markdown() {
-  return {};
+import type { Plugin } from "vite";
+
+export function markdown(): Plugin {
+  return {
+    name: "vite-plugin-markdown",
+    transform(code, id) {
+      if (id.endsWith(".md")) {
+        const transformed = `
+          const attributes = {
+            title: "",
+            time: "",
+            tags: [],
+          };
+          const html = "";
+          export { attributes, html };
+        `;
+        return { code: transformed };
+      }
+      return { code, map: null };
+    },
+  };
 }
