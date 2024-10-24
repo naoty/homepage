6a0b8841756b2fc5e3bd8c0667ba2db7556f8d33
Naoto Kaneko <naoty.k@gmail.com>
2024-10-24 20:43:37 +0900

    remarkを使ってmarkdownをHTMLに変換する

--- packages/vite-plugin-markdown/src/index.ts
+++ packages/vite-plugin-markdown/src/index.ts
@@ -1,20 +1,32 @@
+import rehypeStringify from "rehype-stringify";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import { unified } from "unified";
 import type { Plugin } from "vite";
+
+const processor = unified()
+  .use(remarkParse)
+  .use(remarkRehype)
+  .use(rehypeStringify);
 
 export function markdown(): Plugin {
   return {
     name: "vite-plugin-markdown",
-    transform(code, id) {
+    async transform(code, id) {
       if (id.endsWith(".md")) {
-        const transformed = `
-          const attributes = {
-            title: "",
-            time: "",
-            tags: [],
-          };
-          const html = "";
-          export { attributes, html };
-        `;
-        return { code: transformed };
+        const lines = [];
+        const processed = await processor.process(code);
+        lines.push(`const html = ${JSON.stringify(processed.toString())}`);
+
+        const attributes = {
+          title: "",
+          time: "",
+          tags: [],
+        };
+        lines.push(`const attributes = ${JSON.stringify(attributes)}`);
+        lines.push(`export { html, attributes }`);
+
+        return { code: lines.join("\n") };
       }
       return { code, map: null };
     },
