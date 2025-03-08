ec57c7580f055e21504bf4f6834aad98d55bb368
Naoto Kaneko <naoty.k@gmail.com>
2025-03-08 22:58:36 +0900

    記事詳細のレイアウトを変更する

--- app/routes/posts/post.tsx
+++ app/routes/posts/post.tsx
@@ -1,4 +1,3 @@
-import classes from "~/content.module.css";
 import { Route } from "./+types/post";
 
 export async function loader({ params }: Route.LoaderArgs): Promise<Post> {
@@ -32,17 +31,16 @@ export default function Post({
   loaderData: { attributes, html },
 }: Route.ComponentProps) {
   return (
-    <>
+    <main className="mx-auto max-w-2xl space-y-8">
       <header className="space-y-1">
-        <h1 className="text-2xl font-bold">{attributes.title}</h1>
+        <h1 className="bg-black text-2xl font-bold text-white">
+          {attributes.title}
+        </h1>
         <p className="text-sub text-sm">
           <time dateTime={attributes.time}>{attributes.time}</time>
         </p>
       </header>
-      <article
-        className={classes.post}
-        dangerouslySetInnerHTML={{ __html: html }}
-      />
-    </>
+      <article className="post" dangerouslySetInnerHTML={{ __html: html }} />
+    </main>
   );
 }
