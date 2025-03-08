03ccb567c460926e690513bed086a6985a928918
Naoto Kaneko <naoty.k@gmail.com>
2025-03-08 22:17:12 +0900

    記事一覧を年ごとにまとめる

--- app/routes/posts/home.tsx
+++ app/routes/posts/home.tsx
@@ -14,7 +14,25 @@ export function loader() {
       return { id, ...attributes };
     })
     .sort((a, b) => b.id - a.id);
-  return { attributes };
+
+  const attributesByYear = attributes.reduce<Map<number, PostAttribute[]>>(
+    (result, attribute) => {
+      const timestamp = Date.parse(attribute.time);
+      const year = new Date(timestamp).getFullYear();
+
+      const attributes = result.get(year) ?? [];
+      result.set(year, [...attributes, attribute]);
+
+      return result;
+    },
+    new Map(),
+  );
+
+  const sortedAttributesByYear = Array.from(attributesByYear.entries()).sort(
+    (a, b) => b[0] - a[0],
+  );
+
+  return { attributes, sortedAttributesByYear };
 }
 
 export const meta = () => {
@@ -44,13 +62,22 @@ export default function Home({ loaderData }: Route.ComponentProps) {
   return (
     <main className="mx-auto max-w-2xl space-y-8">
       <h1 className="text-3xl font-bold">Posts</h1>
-      <ul className="list-disc space-y-1 pl-6">
-        {loaderData.attributes.map((attribute) => (
-          <li key={attribute.id}>
-            <Link href={`/posts/${attribute.id}`}>{attribute.title}</Link>
-          </li>
+      <div className="space-y-4">
+        {loaderData.sortedAttributesByYear.map(([year, attributes]) => (
+          <section key={year} className="space-y-2">
+            <h2 className="border-border border-b text-lg">{year}</h2>
+            <ul className="space-y-1 pl-6">
+              {attributes.map((attribute) => (
+                <li key={attribute.id} className="list-disc">
+                  <Link href={`/posts/${attribute.id}`} className="text-link">
+                    {attribute.title}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </section>
         ))}
-      </ul>
+      </div>
     </main>
   );
 }
