349353183b210ef2f979d26277afb0e56a35577b
Naoto Kaneko <naoty.k@gmail.com>
2025-03-11 22:31:29 +0900

    スタイルを微修正する

--- app/routes/posts.tsx
+++ app/routes/posts.tsx
@@ -7,7 +7,7 @@ export default function Posts() {
       <div className="bg-primary dark:bg-dark-primary flex-1 p-4 md:min-h-screen md:p-12">
         <Outlet />
       </div>
-      <nav className="bg-secondary dark:bg-dark-secondary w-3xs space-y-4 p-4 md:min-h-screen md:py-12">
+      <nav className="bg-secondary dark:bg-dark-secondary w-full space-y-4 p-4 md:w-3xs md:min-h-screen md:py-12">
         <h2 className="text-xl">Links</h2>
         <ul className="flex flex-row gap-x-2 md:flex-col md:gap-y-1">
           <li>
